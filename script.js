f90e2073f4e6946ec9370f6fa05b48411f06c2f2
taisatavares <taisa.tavares93@gmail.com>
2025-01-07 11:41:09 -0300

    corrigindo scripts

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.getElementById('search-btn').addEventListener('click', function() {
-    const searchTerm = document.getElementById('search').value.toLowerCase(); // Obtém o valor da barra de pesquisa
-    const gridItems = document.querySelectorAll('.picture'); // Seleciona todos os itens da grid
-    let hasResults = false; // Variável para verificar se há resultados
+    const searchTerm = document.getElementById('search').value.toLowerCase(); 
+    const gridItems = document.querySelectorAll('.picture'); 
+    let hasResults = false; 
   
     gridItems.forEach(item => {
-      const keyword = item.getAttribute('data-keyword').toLowerCase(); // Obtém o valor de data-keyword
+      const keyword = item.getAttribute('data-keyword').toLowerCase(); 
       if (keyword.includes(searchTerm)) {
-        item.style.display = 'block'; // Exibe o item se a palavra-chave corresponder
-        hasResults = true; // Marca que encontrou pelo menos um resultado
+        item.style.display = 'block'; 
+        hasResults = true; 
       } else {
-        item.style.display = 'none'; // Oculta o item se não houver correspondência
+        item.style.display = 'none';  
       }
     });
   
-    // Exibe ou oculta a mensagem de "Nenhuma foto encontrada"
+    
     const noResultsMessage = document.getElementById('no-results');
     if (hasResults) {
-      noResultsMessage.style.display = 'none'; // Oculta a mensagem de "nenhuma foto encontrada"
+      noResultsMessage.style.display = 'none'; 
     } else {
-      noResultsMessage.style.display = 'block'; // Exibe a mensagem de "nenhuma foto encontrada"
+      noResultsMessage.style.display = 'block'; 
     }
   });
 
